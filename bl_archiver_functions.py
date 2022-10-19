8d1d15a4ea51d3b434f0d9e9731afef9baa14c50
Modicolitor <Bjoern.Eckhardt@gmx.de>
2022-10-19 16:01:08 +0200

    cheating for quick task of rendering for dad

--- bl_archiver_functions.py
+++ bl_archiver_functions.py
@@ -1,4 +1,6 @@
 import bpy
+import os
+from .bl_archiver_properties import Bl_Archiver_PropGroup
 
 from .vse_arch_functions import collect_moviclips, collect_sounds,collect_images
 '''
@@ -24,23 +26,41 @@ from bpy.app.handlers import persistent
 imgseq_directories = {}
 blends_to_check = {}'''
 
+all_filepathes = {"filepathes": {}, 
+    "imgseq_directories": {},
+    "blends_to_check": [], 
+    "blends_to_copy": {},
+    "main_filepath": "",
+    }
+
 @persistent
-def load_handler(context: bpy.context, all_filepathes):
-    print(f'in new file all filepathes {all_filepathes}')
+def load_handler(context: bpy.context):#all_filepathes
     blends_to_check = all_filepathes['blends_to_check']
-    blends_to_copy = all_filepathes['blends_to_copy']
-    filepathes = all_filepathes['filepathes']
-    imgseq_directories = all_filepathes['imgseq_directories']
+    #gen_properties(context)
+    #print(f'in new file all filepathes {all_filepathes}')
+    #blends_to_check = all_filepathes['blends_to_check']
+    #blends_to_copy = all_filepathes['blends_to_copy']
+    #filepathes = all_filepathes['filepathes']
+    #imgseq_directories = all_filepathes['imgseq_directories']
     
     
     
     
-    filepathes = collect_moviclips(context, filepathes)
-    filepathes = collect_sounds(context, filepathes)
-    filepathes, imgseq_directories = collect_images(context, filepathes, imgseq_directories)
-    print(f'In other file {filepathes}')
-   
-    return all_filepathes
+    #filepathes = collect_moviclips(context, filepathes)
+    #filepathes = collect_sounds(context, filepathes)
+    #filepathes, imgseq_directories = collect_images(context, filepathes, imgseq_directories)
+    #print(f'In other file {filepathes}')
+    print("some result in ")
+    res = [665+1]
+    
+    
+    own_filepath = bpy.path.abspath('//') # os.path.abspath(bpy.path.abspath('//'))
+    print(f'I"m subs own_filepath {own_filepath}')
+    print(f'blends to check in sub {blends_to_check}')
+    #if not bpy.context.scene.bl_archiver.is_main_file:
+    if own_filepath in blends_to_check:
+        bpy.ops.wm.window_close()
+    return res # all_filepathes
 
 bpy.app.handlers.load_post.append(load_handler)
 
@@ -48,11 +68,18 @@ bpy.app.handlers.load_post.append(load_handler)
 
 
 def start_archiving(context):
-    all_filepathes = {"filepathes": {}, 
+    
+    #set controll bool to indentify main file
+    context.scene.bl_archiver.is_main_file = True
+    
+    own_filepath = bpy.path.abspath('//')
+    all_filepathes['main_filepath'] = own_filepath
+    
+    '''all_filepathes = {"filepathes": {}, 
     "imgseq_directories": {},
     "blends_to_check": [], 
     "blends_to_copy": {},
-    }
+    }'''
     
     blends_to_check = all_filepathes['blends_to_check']
     
@@ -61,19 +88,21 @@ def start_archiving(context):
         if hasattr(lib, 'filepath'):
             if lib.filepath not in blends_to_check:
                 blends_to_check.append(lib.filepath)
-
+    print(f'found libaries {blends_to_check}')
     #it should be a while loop until all to check are worked through
     while len(blends_to_check) != 0: 
-        all_filepathes = get_filepathes_from_blends(all_filepathes)
+        get_filepathes_from_blends()
 
 
     print(blends_to_check)
     #print(filepathes)
     #print(imgseq_directories)
 
+def gen_properties(context):
+    bpy.types.Scene.bl_archiver = bpy.props.PointerProperty(type=Bl_Archiver_PropGroup)
 
 #checks first item of filepath and removes it afterwards
-def get_filepathes_from_blends(all_filepathes):
+def get_filepathes_from_blends():
     blends_to_check = all_filepathes['blends_to_check']
     blends_to_copy = all_filepathes['blends_to_copy']
     filepathes = all_filepathes['filepathes']
@@ -83,19 +112,27 @@ def get_filepathes_from_blends(all_filepathes):
 
     #filepathes, imgseq_directories = open_new_blend(filepath, filepathes, imgseq_directories)
     print(f'before start{all_filepathes}')
+    print(f'I"m mains own_filepath {all_filepathes}')
+    
     use_instance = True
     if use_instance:
         import subprocess
-        try:
-            subprocess.Popen(bpy.app.binary_path, filepath, all_filepathes)
-        except:
+        #try:
+        #settings["linked_file"] = os.path.abspath(bpy.path.abspath(targetpath))
+        print(f'starting sub file {filepath}')
+        p = subprocess.Popen([bpy.app.binary_path, os.path.abspath(bpy.path.abspath(filepath)), '-b']) # , all_filepathes
+        #
+        out, err = p.communicate()
+        #print(f'out of subprocess {out}')
+        #print(f'error of subprocess {err}')
+        '''except:
             print('Error') ### needs better error handling
             #logger.error("Error on the new Blender instance")
             #import traceback
-            #logger.error(traceback.print_exc())
-    else:
-        bpy.ops.wm.open_mainfile(filepath=filepath)
+            #logger.error(traceback.print_exc())'''
+    #else:
+        #bpy.ops.wm.open_mainfile(filepath=filepath)
 
     ###important for stoping the while loop 
     blends_to_check.remove(filepath)
-    return blends_to_check, filepathes, imgseq_directories
+    return all_filepathes # blends_to_check, filepathes, imgseq_directories
--- bl_archiver_properties.py
+++ bl_archiver_properties.py
@@ -10,3 +10,6 @@ class Bl_Archiver_PropGroup(bpy.types.PropertyGroup):
 
     use_blend_data: bpy.props.BoolProperty(
         name="Include Blend Data", default=True)
+    
+    is_main_file: bpy.props.BoolProperty(
+        name="Is Main File", default=False, description="used to close the right files and not the main")
